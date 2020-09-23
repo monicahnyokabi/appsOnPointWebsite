b5fd5097f0861dcdce56d5fe8abb91ef81073727
monicahnyokabi <wambuinyokabimn@gmail.com>
2020-09-23 05:04:36 +0300

    implemented design :rocket: :relaxed:

--- src/App.js
+++ src/App.js
@@ -5,20 +5,222 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div class="container-fluid  main-section">
+
+        <div class="row section-part">
+          <div class="col-md-12">
+            <h1 class="float-left">Welcome To Our Creativity Hub!</h1>
+          </div>
+          <div class="col-md-12">
+            <p class="float-left">
+              IT'S NICE TO MEET YOU
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          </div>
+          <div class="col-md-12">
+            <button class="btn btn-light">What do we do?</button>
+          </div>
+        </div>
+        <i class="fa fa-angle-double-down blink" aria-hidden="true"></i>
+      </div>
+      <div class="container-fluid ">
+
+        <div class="d-flex justify-content-center mt-5 mb-3">
+          <h3 class="rcorners3 text-white text-center">Services</h3>
+        </div>
+
+
+
+        <div class="row  mt-5">
+
+          <div class="col-md-4">
+            <div class="row">
+              <div class="col-md-2 mr-5">
+                <img src="/mobile.PNG" alt="" />
+              </div>
+              <div class="col-md-8">
+                <h4>Mobile Applications</h4>
+                <p>
+                  We create elegant mobile applications with stunning features
+                  that will give you are wider reach and show your clients how
+                  far you are willing to go to serve them better.
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-md-4">
+            <div class="row">
+              <div class="col-md-2 mr-5">
+                <img src="/Annotation 2020-09-21 163901.png" alt="" />
+              </div>
+              <div class="col-md-8">
+                <h4>Responsive Web Design</h4>
+                <p>
+                  We design, develop, commission and maintain fast, beautiful and websites that will look
+                  great on any
+                  screen and keep your visitors coming back.
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-md-4">
+            <div class="row">
+              <div class="col-md-2 mr-5">
+                <img src="/Annotation 2020-09-21 164340.png" alt="" />
+              </div>
+              <div class="col-md-8">
+
+                <h4>Security</h4>
+                <p>
+                  We carry out through penetration tests and identify potential vulnerabitity in your network
+                  infrastructure; hardware, software and dependent resources inclusive. Our team of security
+                  experts
+                  will plug these weaknesses, and provide continuous guidance on how to minimize creating
+                  others as
+                  your resources grow.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row mt-5">
+          <div class="col-md-4">
+            <div class="row">
+              <div class="col-md-2 mr-5">
+                <img src="/Annotation 2020-09-21 170012.png" alt="" />
+              </div>
+              <div class="col-md-8">
+                <h4>Scalability & Performance</h4>
+                <p>
+                  We pride ourselves in being able to make your application handle millions of user requests
+                  as
+                  quickly and efficiently as it handles a single user. This is vital in creating a pleasant
+                  user
+                  experience, and keep them loyal to you.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="row">
+              <div class="col-md-2 mr-5">
+                <img src="/Annotation 2020-09-21 171708.png" alt="" />
+              </div>
+              <div class="col-md-8">
+                <h4>System & Database Administration</h4>
+                <p>
+                  We will manage your server installation, topology and configuration, minimize your downtimes
+                  making
+                  your services available at user convenience and keep your clientelle happy.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="row">
+              <div class="col-md-2 mr-5">
+                <img src="/Annotation 2020-09-21 171752.png" alt="" />
+              </div>
+
+              <div class="col-md-8">
+
+                <h4>IT Consultancy</h4>
+
+                <p>
+                  Need guidance on how to handle problems in the computer technology sphere? Talk to us. We
+                  will put
+                  all the knowledge and years of experience to use to help you in the right direction.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="d-flex justify-content-center mt-5 mb-3">
+          <h3 class="rcorners3 text-white text-center">About</h3>
+        </div>
+
+        <div class="d-flex justify-content-center mt-5 mb-3">
+          <h5>A few facts about Apps On Point LTD.</h5>
+        </div>
+
+
+
+        <div class="row">
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <h4 class="text-center">WHY APPS ON POINT?</h4>
+                <h5 class="text-center">We keep your applications on point</h5>
+
+                <p>
+                  Our expertise reach covers all the areas that are needed to keep consumer applications
+                  running optimaly and flawlessly. We handle application design, development, debugging,
+                  installation, maintainance, documentation, upgrading, security, database management, system
+                  tuning and scaling. In addition, the term 'Apps' shows that we are modern and aware of the
+                  changing trends in both the software technology and user circles.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6" height="400">
+            <div class="card">
+              <div class="card-body">
+                <h4>MISSION</h4>
+                <h5>Client satisfaction</h5>
+                <p>
+                  At Apps On Point limited, we strive to keep you happy to work with us, by being exceptional
+                  at our work, exceeding your expectations and providing excellent customer relations.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="d-flex justify-content-center  mt-5 mb-3 ">
+          <h3 class="rcorners3 text-white text-center">Team</h3>
+        </div>
+
+
+        <div class="row">
+          <div class="col-md-3">
+            <img src="/avataaars.png" height="200" width="200" alt="" />
+          </div>
+          <div class="col-md-3">
+            <img src="/avataaars.png" height="200" width="200" alt="" />
+          </div>
+          <div class="col-md-3">
+            <img src="/avataaars.png" height="200" width="200" alt="" />
+          </div>
+          <div class="col-md-3">
+            <img src="/avataaars.png" height="200" width="200" alt="" />
+          </div>
+        </div>
+
+        <div class="d-flex justify-content-center mt-5 mb-3">
+          <h3 class="rcorners3 text-white text-center">Contact</h3>
+        </div>
+
+
+        <footer class="bg-dark text-white mt-4">
+          <div class="row">
+            <div class="col-md-4">
+
+            </div>
+            <div class="col-md-4">
+
+            </div>
+            <div class="col-md-4 mt-5 p-5">
+              <img src="/Annotation 2020-09-21 234646_1.png" alt="" />
+            </div>
+          </div>
+        </footer>
+
+      </div>
     </div>
   );
 }
